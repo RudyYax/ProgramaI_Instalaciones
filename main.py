4f035b5cc80b31b456ecb19fea2f7e600a8ed819
RudyYax <78948207+RudyYax@users.noreply.github.com>
2026-08-22 17:13:51 -0600

    hola

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def abrir_panel_principal(nombre):
         command=ventana.destroy,
         bg="red",
         fg="white",
-        font=("Arial", 4),
+        font=("Arial", 2),
         width=25
     )
     lienzo.create_window(ancho // 2, 200, window=boton_salir)
