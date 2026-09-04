23164e9d8318d8f33b39fd4d15b8f01722ba1274
Enrique <enriquevelasquez354@gmail.com>
2026-09-04 00:55:40 -0600

    TUBOCONDUIT

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ def ventana_caida_tension(padre):
         font=("Arial", 16, "bold")
     ).grid(row=0, column=0, columnspan=2, pady=15)
 
-    tk.Label(ventana, text="ΔV = (p * L * I) / A", font=("Arial", 10), fg="gray").grid(row=1, column=0, columnspan=2)
+    tk.Label(ventana, text="ΔV = (2*p * L * I) / A", font=("Arial", 10), fg="gray").grid(row=1, column=0, columnspan=2)
 
 
     campos = {
